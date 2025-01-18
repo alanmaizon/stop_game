cf21ae5be2c1ab43710b6ff1b76281f331a81200
Alan Maizon <alanmaizon@hotmail.com>
2025-01-18 11:00:10 +0000

    modified:   stop_game/settings.py

--- stop_game/settings.py
+++ stop_game/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
 ]
 
 # For deployment
-STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
