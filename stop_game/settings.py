3ed3e4c8dd237049569e9b0ae11b05c00b9a05c8
Alan Maizon <alanmaizon@hotmail.com>
2025-01-18 11:03:10 +0000

    modified:   stop_game/settings.py

--- stop_game/settings.py
+++ stop_game/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'stop_game.urls'
@@ -143,6 +144,8 @@ STATICFILES_DIRS = [
 
 # For deployment
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
