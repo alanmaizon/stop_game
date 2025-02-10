57fcae7c63ae4c2239cb506697b7b093b23489f5
Alan Maizon <alanmaizon@hotmail.com>
2025-02-10 23:56:06 +0000

    debug

--- user/models.py
+++ user/models.py
@@ -1,4 +1,12 @@
 from django.core.files.storage import default_storage
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.conf import settings
+
+def user_avatar_upload_path(instance, filename):
+    """Ensure avatars are stored as avatars/username.extension"""
+    extension = filename.split('.')[-1]
+    return f"avatars/{instance.username}.{extension}"
 
 class User(AbstractUser):
     ROLE_CHOICES = (
@@ -11,16 +19,15 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
-def delete_old_avatar(self):
-    """Elimina el avatar anterior antes de actualizarlo."""
-    if self.pk:
-        try:
-            old_instance = User.objects.get(pk=self.pk)
-            if old_instance.avatar and old_instance.avatar.name != self.avatar.name:
-                print(f"Eliminando: {old_instance.avatar.name}")  # <--- Debugging
-                default_storage.delete(old_instance.avatar.name)
-        except User.DoesNotExist:
-            pass
+    def delete_old_avatar(self):
+        """Elimina el avatar anterior antes de actualizarlo."""
+        if self.pk:  # Asegura que el usuario ya existe en la BD
+            try:
+                old_instance = User.objects.get(pk=self.pk)
+                if old_instance.avatar and old_instance.avatar.name != self.avatar.name:
+                    default_storage.delete(old_instance.avatar.name)  # Usar django-storages
+            except User.DoesNotExist:
+                pass
 
     def save(self, *args, **kwargs):
         """Sobreescribe save para eliminar el avatar antiguo antes de guardar el nuevo."""
