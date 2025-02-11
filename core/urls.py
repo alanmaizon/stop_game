be3753a2c4adbff4ecdb79917fb8c1172acc27d1
Alan Maizon <alanmaizon@hotmail.com>
2025-02-11 00:25:57 +0000

    debug

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home_page
+from .views import home_page, debug_static_media
 
 urlpatterns = [
     path('', home_page, name='home'),
+    path("debug-static/", debug_static_media),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.http import JsonResponse
+from django.conf import settings
 
 def home_page(request):
     """Home Page View"""
     if request.user.is_authenticated:
         return redirect('game:lobby')
     return render(request, 'core/index.html')
+
+def debug_static_media(request):
+    return JsonResponse({
+        "DEBUG": settings.DEBUG,
+        "STATIC_URL": settings.STATIC_URL,
+        "STATIC_ROOT": settings.STATIC_ROOT,
+        "MEDIA_URL": settings.MEDIA_URL,
+        "MEDIA_ROOT": settings.MEDIA_ROOT,
+        "STATICFILES_DIRS": settings.STATICFILES_DIRS if hasattr(settings, 'STATICFILES_DIRS') else None,
+    })
